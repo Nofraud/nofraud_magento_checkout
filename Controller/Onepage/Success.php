f19db5cb78c8ccc5145cddd04ecc12b16999aa02
sam <sam@kammalou.com>
2023-10-23 17:01:21 +0530

    feat: Support redirection to native thank you page
    feat: worked on the thank you page redirection
    
    feat: worked on the thank you page redirection
    
    feat: fixed issue with session at thank you page

--- Controller/Onepage/Success.php
+++ Controller/Onepage/Success.php
@@ -8,30 +8,26 @@ class Success extends OriginalSuccess
 {
     public function execute()
     {
-        try {
-            $orderId = $this->getRequest()->getParam('orderId');
-            $session = $this->getOnepage()->getCheckout();
-            $resultPage = $this->resultPageFactory->create();
-            if ($orderId) {
-                $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-                $order = $objectManager->get(\Magento\Sales\Api\Data\OrderInterface::class)->loadByIncrementId($orderId);
-                $session->setLastOrderId($order->getId());
-                $session->setLastRealOrderId($orderId);
-            } else {
-                if (!$this->_objectManager->get(\Magento\Checkout\Model\Session\SuccessValidator::class)->isValid()) {
-                    return $this->resultRedirectFactory->create()->setPath('checkout/cart');
-                }
-                $session->clearQuote();
-                $this->_eventManager->dispatch(
-                    'checkout_onepage_controller_success_action',
-                    [
-                        'order_ids' => [$session->getLastOrderId()],
-                        'order' => $session->getLastRealOrder()
-                    ]
-                );
+        $orderId = $this->getRequest()->getParam('orderId');
+        $session = $this->getOnepage()->getCheckout();
+        $resultPage = $this->resultPageFactory->create();
+        if ($orderId) {
+            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+            $order = $objectManager->create('\Magento\Sales\Model\OrderRepository')->get($orderId);
+            $session->setLastOrderId($order->getIncrementId());
+            $session->setLastRealOrderId($order->getIncrementId());
+        } else {
+            if (!$this->_objectManager->get(\Magento\Checkout\Model\Session\SuccessValidator::class)->isValid()) {
+                return $this->resultRedirectFactory->create()->setPath('checkout/cart');
             }
-        } catch (\Exception $e) {
-            error_log("\n " . $e->getMessage(), 3, BP . "/var/log/CheckoutSuccessLog.log");
+            $session->clearQuote();
+            $this->_eventManager->dispatch(
+                'checkout_onepage_controller_success_action',
+                [
+                    'order_ids' => [$session->getLastOrderId()],
+                    'order' => $session->getLastRealOrder()
+                ]
+            );
         }
         return $resultPage;
     }
