38557ffc1e980f9b757389696ca4f096cfe10ce6
sam <sam@kammalou.com>
2023-10-25 18:44:04 +0530

    feat: fixed issue with session at thank you page

--- Controller/Onepage/Success.php
+++ Controller/Onepage/Success.php
@@ -1,43 +1,34 @@
 <?php
+
 namespace NoFraud\Checkout\Controller\Onepage;
+
 use Magento\Checkout\Controller\Onepage\Success as OriginalSuccess;
 
 class Success extends OriginalSuccess
 {
-    protected $order;
-
-    public function __construct(
-        \Magento\Sales\Api\Data\OrderInterface $order
-    ) {
-        $this->order = $order;
-    }
-
     public function execute()
     {
         $orderId = $this->getRequest()->getParam('orderId');
         $session = $this->getOnepage()->getCheckout();
-        if ($orderId) {
-            $order = $this->order->load($orderId);
-            $session->setLastOrderId($order->getIncrementId());
-            $session->setLastRealOrder($order);
-            $session->setLastQuoteId($order->getQuoteId());
-            $session->setLastSuccessQuoteId($order->getQuoteId());
-        }
-
-        if (!$this->_objectManager->get(\Magento\Checkout\Model\Session\SuccessValidator::class)->isValid()) {
-            return $this->resultRedirectFactory->create()->setPath('checkout/cart');
-        }
-
-        $session->clearQuote();
-
         $resultPage = $this->resultPageFactory->create();
-        $this->_eventManager->dispatch(
-            'checkout_onepage_controller_success_action',
-            [
-                'order_ids' => [$session->getLastOrderId()],
-                'order' => $session->getLastRealOrder()
-            ]
-        );
+        if ($orderId) {
+            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+            $order = $objectManager->create('\Magento\Sales\Model\OrderRepository')->get($orderId);
+            $session->setLastOrderId($order->getIncrementId());
+            $session->setLastRealOrderId($order->getIncrementId());
+        } else {
+            if (!$this->_objectManager->get(\Magento\Checkout\Model\Session\SuccessValidator::class)->isValid()) {
+                return $this->resultRedirectFactory->create()->setPath('checkout/cart');
+            }
+            $session->clearQuote();
+            $this->_eventManager->dispatch(
+                'checkout_onepage_controller_success_action',
+                [
+                    'order_ids' => [$session->getLastOrderId()],
+                    'order' => $session->getLastRealOrder()
+                ]
+            );
+        }
         return $resultPage;
     }
 }
