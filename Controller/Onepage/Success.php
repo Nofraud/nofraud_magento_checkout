45c49f91f2745027e5dc55348782decec43603b0
sam <sam@kammalou.com>
2023-12-21 17:48:17 +0530

    fix: worked on loading order through the increment id at the success page

--- Controller/Onepage/Success.php
+++ Controller/Onepage/Success.php
@@ -8,26 +8,30 @@ class Success extends OriginalSuccess
 {
     public function execute()
     {
-        $orderId = $this->getRequest()->getParam('orderId');
-        $session = $this->getOnepage()->getCheckout();
-        $resultPage = $this->resultPageFactory->create();
-        if ($orderId) {
-            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-            $order = $objectManager->create('\Magento\Sales\Model\OrderRepository')->get($orderId);
-            $session->setLastOrderId($order->getIncrementId());
-            $session->setLastRealOrderId($order->getIncrementId());
-        } else {
-            if (!$this->_objectManager->get(\Magento\Checkout\Model\Session\SuccessValidator::class)->isValid()) {
-                return $this->resultRedirectFactory->create()->setPath('checkout/cart');
+        try {
+            $orderId = $this->getRequest()->getParam('orderId');
+            $session = $this->getOnepage()->getCheckout();
+            $resultPage = $this->resultPageFactory->create();
+            if ($orderId) {
+                $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+                $order = $objectManager->get(\Magento\Sales\Api\Data\OrderInterface::class)->loadByIncrementId($orderId);
+                $session->setLastOrderId($order->getId());
+                $session->setLastRealOrderId($orderId);
+            } else {
+                if (!$this->_objectManager->get(\Magento\Checkout\Model\Session\SuccessValidator::class)->isValid()) {
+                    return $this->resultRedirectFactory->create()->setPath('checkout/cart');
+                }
+                $session->clearQuote();
+                $this->_eventManager->dispatch(
+                    'checkout_onepage_controller_success_action',
+                    [
+                        'order_ids' => [$session->getLastOrderId()],
+                        'order' => $session->getLastRealOrder()
+                    ]
+                );
             }
-            $session->clearQuote();
-            $this->_eventManager->dispatch(
-                'checkout_onepage_controller_success_action',
-                [
-                    'order_ids' => [$session->getLastOrderId()],
-                    'order' => $session->getLastRealOrder()
-                ]
-            );
+        } catch (\Exception $e) {
+            error_log("\n " . $e->getMessage(), 3, BP . "/var/log/CheckoutSuccessLog.log");
         }
         return $resultPage;
     }
